5de2be701179b2afe6d454f7dbf9bb69dd038741
AlSayuri <hashimura13@gmail.com>
2024-09-08 19:02:04 -0300

    State

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,26 +1,13 @@
-import React from "react";
 import style from './Lista.module.scss';
 import Item from "./Item";
+import { ITarefa } from '../../types/tarefa';
 
-function Lista(){
-    const tarefas = [
-        {
-            tarefa: "React",
-            tempo: '02:00:00'
-        },
-        {
-            tarefa: "JavaScript",
-            tempo: '01:00:00'
-        },
-        {
-            tarefa: "Typescript",
-            tempo: '03:00:00'
-        }
-    ];
-
+function Lista({tarefas} : {tarefas: ITarefa[]}){
     return(
         <aside className={style.listaTarefas}>
-            <h2>Estudo do dia</h2>
+            <h2>
+                Estudo do dia
+            </h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <Item 
